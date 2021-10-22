77cb698bee9e25e98c2876f9e05c5605660ad326
Akshay Nalawade <akshaybest3120@gamil.com>
2021-10-22 15:34:50 +0530

    code edited through vs code

--- types.py
+++ types.py
@@ -1,1 +1,15 @@
-print("working today too")
+import random
+
+def game():
+    hand = str(input())
+    random_hand = random.choice(['rock','paper','secior'])
+    while hand.lower() == random_hand:
+        print(f"you win you enterd {hand} and computer choosed {random_hand} ")
+        break    
+    else:
+        print("try again")
+        hand = str(input())
+
+
+game()
+        
