ce0680f699e17f0608d0ac130f2afb70b87275d2
Akshay Nalawade <akshaybest3120@gamil.com>
2021-10-22 15:53:33 +0530

    final code from video tutorial

--- project.py
+++ project.py
@@ -1,15 +1,29 @@
 import random
 
-def game():
-    hand = str(input())
-    random_hand = random.choice(['rock','paper','secior'])
-    while hand.lower() == random_hand:
-        print(f"you win you enterd {hand} and computer choosed {random_hand} ")
-        break    
+while True:
+    hand = str(input()).lower()
+
+    if hand == "quit":
+        break
+
+    if hand != 'rock' and hand != 'paper' and hand != 'scissor':
+        print("enter correct input")
+        continue
+
+    computer_hand = random.choice(['rock','paper','scissor'])
+    print(f"computer choosed {computer_hand} ")
+
+    if computer_hand == hand:
+        print("Tie")
+        continue
+    elif hand == 'paper' and computer_hand =='rock':
+        print("you won")
+        break
+    elif hand == 'rock' and computer_hand =='scissor':
+        print("you won")
+        break
+    elif hand == 'scissor' and computer_hand =='paper':
+        print("you won")
+        break
     else:
-        print("try again")
-        hand = str(input())
-
-
-game()
-        
+        print("you lost")
