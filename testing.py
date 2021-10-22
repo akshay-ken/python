27de7d1b70df4ab92870df7e2ac33fd5d8198f97
Akshay Nalawade <akshaybest3120@gamil.com>
2021-10-22 18:35:20 +0530

    adding updated fiels

--- testing.py
+++ testing.py
@@ -1,2 +1,14 @@
-with open('3.2 readme.txt','r') as file:
-    print(file.read())
+filename = input("whats the file name? ")
+content = input("Enter content you want to add")
+
+with open(filename,'w') as file:
+    file.write(content)
+
+file_open = input('enter y or n')
+
+if file_open in ['y','n']:
+    if file_open == 'y':
+        with open(filename,'r') as file:
+            print(file.read())
+    else:
+        print('closed and saved')
