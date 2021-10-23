f89f4d9438099cd938c8db2ed8bf1e608870aaa7
Akshay Nalawade <akshaybest3120@gamil.com>
2021-10-23 22:33:16 +0530

    completed todays project

--- project.py
+++ project.py
@@ -1,15 +1,21 @@
 import requests
 
-pokemon = input("Enter you favorite pokemon NAME! ")
-pokemon = pokemon.lower()
+while True:
+    pokemon = input("Enter you favorite pokemon NAME! ")
+    pokemon = pokemon.lower()
 
-url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
 
-req = requests.get(url)
+    req = requests.get(url)
+    if pokemon == 'quit':
+        break
+    if req.status_code == 200:
+        data = req.json()
 
-data = req.json()
-
-print(f"The choosed pokemon NAME is {data['name']}")
-print("And this pokemon have abilities of : ")
-for ability in data['abilities']:
-    print(ability['ability']['name'])
+        print(f"The choosed pokemon NAME is {data['name']}")
+        print("And this pokemon have abilities of : ")
+        for ability in data['abilities']:
+            print(ability['ability']['name'])
+    else:
+        print('enter name properly')
+        pokemon = input()
