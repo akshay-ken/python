3d72b956996338a4fa40e9530117cb4692753170
akshay-ken <43084414+akshay-ken@users.noreply.github.com>
2021-10-28 20:55:03 +0530

    Delete mysite directory

--- mysite/feed/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomePageView, PostDetailView
-
-app_name = 'feed'
-
-urlpatterns = [
-    path('',HomePageView.as_view(), name = 'index'),
-    path('detail/<int:pk>/', PostDetailView.as_view(), name = 'detail'),
-]
